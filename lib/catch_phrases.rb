1d5c73d902abc26ee02db3cbfddc84978fb5a07a
Hannah Drury <hannah_drury@yahoo.com>
2020-01-17 05:40:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,19 @@
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
   puts status
+end
+
+def 
+
+end
+
+def
+
 end
